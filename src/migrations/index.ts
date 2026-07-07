064f42867ea3a4f534424163b587fdf96feac597
Syed W. Azeem <syed.w.azeem@live.com>
2026-07-07 09:22:34 -0400

    Clean up orphaned summer-post media left by the pre-restore delete

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260703_032255_initial from './20260703_032255_initial'
 import * as migration_20260706_000000_restore_summer_posts from './20260706_000000_restore_summer_posts'
+import * as migration_20260707_000000_cleanup_orphan_summer_media from './20260707_000000_cleanup_orphan_summer_media'
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260706_000000_restore_summer_posts.up,
     down: migration_20260706_000000_restore_summer_posts.down,
     name: '20260706_000000_restore_summer_posts',
+  },
+  {
+    up: migration_20260707_000000_cleanup_orphan_summer_media.up,
+    down: migration_20260707_000000_cleanup_orphan_summer_media.down,
+    name: '20260707_000000_cleanup_orphan_summer_media',
   },
 ]
