2d538312b7903cd2ce2fbe57155b8db8165164bf
Syed W. Azeem <syed.w.azeem@live.com>
2026-07-07 10:36:25 -0400

    Retry orphan summer-media cleanup with hardened deletes (#186)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260703_032255_initial from './20260703_032255_initial'
 import * as migration_20260706_000000_restore_summer_posts from './20260706_000000_restore_summer_posts'
 import * as migration_20260707_000000_cleanup_orphan_summer_media from './20260707_000000_cleanup_orphan_summer_media'
+import * as migration_20260707_010000_cleanup_orphan_summer_media_retry from './20260707_010000_cleanup_orphan_summer_media_retry'
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260707_000000_cleanup_orphan_summer_media.up,
     down: migration_20260707_000000_cleanup_orphan_summer_media.down,
     name: '20260707_000000_cleanup_orphan_summer_media',
+  },
+  {
+    up: migration_20260707_010000_cleanup_orphan_summer_media_retry.up,
+    down: migration_20260707_010000_cleanup_orphan_summer_media_retry.down,
+    name: '20260707_010000_cleanup_orphan_summer_media_retry',
   },
 ]
